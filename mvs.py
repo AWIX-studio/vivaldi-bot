5f7d3fd2d813ea70b9b129a95be647f7759afb1b
Av10n <god.boy13243546@gmail.com>
2025-04-17 21:29:27 +0300

    ой

---
+++ mvs.py
@@ -0,0 +1,59 @@
+
+# coding=utf-8
+
+
+
+
+
+import telebot
+
+
+from telebot import types
+
+
+
+
+
+bot = telebot.TeleBot('7414108235:AAGOilxSXgIVZcXTa4ewGI7DZSPbjx9YP-8')
+
+
+
+
+
+@bot.message_handler(content_types=['text'])
+
+
+def get_text_messages(message):
+
+
+
+
+
+    if message.text == 'расцветает весна в аду':
+
+
+        bot.send_message(message.from_user.id, 'может вместе сдохнем')
+
+
+    elif message.text == 'капли крови в раю':
+
+
+        bot.send_message(message.from_user.id, 'может вместе сдохнем')
+
+
+    elif message.text == 'я уверен что смогу':
+
+
+        bot.send_message(message.from_user.id, 'может вместе сдохнем')
+
+
+    elif message.text == 'убить нас двоих в саду':
+
+
+        bot.send_message(message.from_user.id, 'может вместе сдохнем')
+
+
+
+
+
+bot.polling(none_stop=True, interval=0)
