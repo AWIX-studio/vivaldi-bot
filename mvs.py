9b6d7b29c3d82d7e8f3250d454ef1d637662153c
Herowhither <73470226+Herowhither@users.noreply.github.com>
2025-04-19 00:43:12 +0300

    Сделал распознавание аудиофайлов и немного начальных действий

--- mvs.py
+++ mvs.py
@@ -3,9 +3,18 @@ from telebot import types
 
 bot = telebot.TeleBot('7414108235:AAGOilxSXgIVZcXTa4ewGI7DZSPbjx9YP-8')
 
-@bot.message_handler(commands=['sendfile'])
-def send_document(message):
-    with open('может вместе сдохнем.mp3', 'rb') as audio:
-        bot.send_audio(message.chat.id, audio)
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.from_user.id, 'Привет. Я Вивальди бот. Я создан, чтобы немного упростить тебе работу с музыкой. Пришли мне свой аудиофайл и я скажу, что я могу с ним сделать (или напиши /help чтобы получить информацию по всем моим возможностям).')
+
+@bot.message_handler(content_types=['text','audio', 'voice', 'document'])
+def get_text_messages(message):
+    if message.text == '/help':
+        bot.send_message(message.from_user.id, 'Coming soon...')
+    if message.content_type == 'audio':
+        file_id = message.audio.file_id
+        bot.reply_to(message, 'Получил твою музыку! Слушай, ну и парашу же ты слушаешь, чувак...')
+
+
 
 bot.polling(none_stop=True, interval=0)
